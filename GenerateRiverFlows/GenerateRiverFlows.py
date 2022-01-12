dbec9c5593b8d45c484f1cc713ff5a7db78b56a5
AbelTu0620 <j.tu20130227@gmail.com>
2022-01-12 18:28:13 +0000

    Updated the part of reading 'xlsx' file, make it can read data from a single sheet and save it as a matrix.

--- GenerateRiverFlows/GenerateRiverFlows.py
+++ GenerateRiverFlows/GenerateRiverFlows.py
@@ -13,33 +13,26 @@ import numpy as np
 import xlrd
 import csv
 from datetime import date
-from sklearn import preprocessing
-import pandas as pd
-
 
 # here is converted from part of "GenerateRiverFlowsExample.m", which is used to read data from csv and xlsx files.
 # Specify start time
 t0 = date.toordinal(date(2010,1,1))+366
 
 # Import GEFS weather data. (please notice here, the start sheet‘s number is "0")
-# gefsData = xlrd.open_workbook('/Users/abeltu/Desktop/GenerateRiverFlows/GEFSdata.xlsx').sheets()[16]
-
 def excel_to_matrix(path, sheetNum):
     table = xlrd.open_workbook(path).sheets()[sheetNum]
     row = table.nrows
     col = table.ncols
-    datamatrix = np.zeros((row, col))
-    for x in range(col):
-        cols = np.matrix(table.col_values(x))
-        datamatrix[:, x] = cols
-    min_max_scaler = preprocessing.MinMaxScaler()
-    datamatrix = min_max_scaler.fit_transform(datamatrix)
+    datamatrix = np.zeros((row, col)) # ignore the first title row.
+    for x in range(1,row):
+        row = np.matrix(table.row_values(x))
+        datamatrix[x, :] = row
+    datamatrix = np.delete(datamatrix, 0, axis=0)
     return datamatrix
 
 datafile = u'/Users/abeltu/Desktop/GenerateRiverFlows/GEFSdata.xlsx'
-sheetNum = 16
-excel_to_matrix(datafile,sheetNum)
-
+sheetNum = 0
+gefsData = excel_to_matrix(datafile,sheetNum)
 
 def GenerateRiverFlows( t0, GEFSdata, F0):
     """
