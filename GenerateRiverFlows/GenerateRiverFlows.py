d747a3a48056ae1ccfe49a015bbce13b75d687b2
AbelTu0620 <j.tu20130227@gmail.com>
2022-01-12 17:03:50 +0000

    ""

--- GenerateRiverFlows/GenerateRiverFlows.py
+++ GenerateRiverFlows/GenerateRiverFlows.py
@@ -4,14 +4,41 @@ script, which is used to generate river flows.
                                                             Created by Jiada Tu
                                                             13/01/2022
 """
+
+
+# WARNING： The latest version of xlrd only supports .xls files.
+# Installing the older version 1.2.0 worked for me to open .xlsx files.
+
 import numpy as np
+import xlrd
+import csv
+from datetime import date
+from sklearn import preprocessing
+import pandas as pd
 
 
+# here is converted from part of "GenerateRiverFlowsExample.m", which is used to read data from csv and xlsx files.
+# Specify start time
+t0 = date.toordinal(date(2010,1,1))+366
 
+# Import GEFS weather data. (please notice here, the start sheet‘s number is "0")
+# gefsData = xlrd.open_workbook('/Users/abeltu/Desktop/GenerateRiverFlows/GEFSdata.xlsx').sheets()[16]
 
+def excel_to_matrix(path, sheetNum):
+    table = xlrd.open_workbook(path).sheets()[sheetNum]
+    row = table.nrows
+    col = table.ncols
+    datamatrix = np.zeros((row, col))
+    for x in range(col):
+        cols = np.matrix(table.col_values(x))
+        datamatrix[:, x] = cols
+    min_max_scaler = preprocessing.MinMaxScaler()
+    datamatrix = min_max_scaler.fit_transform(datamatrix)
+    return datamatrix
 
-
-
+datafile = u'/Users/abeltu/Desktop/GenerateRiverFlows/GEFSdata.xlsx'
+sheetNum = 16
+excel_to_matrix(datafile,sheetNum)
 
 
 def GenerateRiverFlows( t0, GEFSdata, F0):
@@ -41,10 +68,10 @@ def GenerateRiverFlows( t0, GEFSdata, F0):
     """
 
     # Specify time-step in days
-    dt = 0.25;
+    dt = 0.25
 
     # Determine number of data points
-    N = np.size(GEFSdata,1);
+    N = np.size(GEFSdata,1)
 
     # Specify date-time in days of each data point
 
