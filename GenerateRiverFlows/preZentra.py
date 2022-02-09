e5424e78bdafbd16b9d71c96f6c3fffaaaea4656
AbelTu0620 <j.tu20130227@gmail.com>
2022-02-09 12:34:22 +0000

    "set the start time to a variable (30 days back)."

--- GenerateRiverFlows/preZentra.py
+++ GenerateRiverFlows/preZentra.py
@@ -2,20 +2,16 @@ from zentra.api import *
 from os import getenv
 import datetime
 
+# obtain start time ( 30 days before)
+backTime = 30
+startTime = datetime.datetime.now() - datetime.timedelta(days=backTime)
 
 # authentication into the Zentra Cloud API
 token = ZentraToken(username=getenv("zentra_un"), password=getenv("zentra_pw"))
 
-
 # Get the readings for a device
 readings = ZentraReadings().get(
-    sn="06-02047",
-    token=token,
-    start_time=int(
-        datetime.datetime(
-            year=2022, month=2, day=9, hour=10, minute=0, second=0
-        ).timestamp()
-    ),
+    sn="06-02047", token=token, start_time=int(startTime.timestamp()),
 )
 zentraData = readings.response
 
