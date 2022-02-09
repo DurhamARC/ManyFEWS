483161e54b451b09ec893bbd069a13c9a81028ed
AbelTu0620 <j.tu20130227@gmail.com>
2022-02-09 12:23:22 +0000

    "extract data sets from zentra device with SN:'06-02047'."

--- GenerateRiverFlows/preZentra.py
+++ GenerateRiverFlows/preZentra.py
@@ -11,9 +11,96 @@ token = ZentraToken(username=getenv("zentra_un"), password=getenv("zentra_pw"))
 readings = ZentraReadings().get(
     sn="06-02047",
     token=token,
-    start_time=int(datetime.datetime(year=2022, month=2, day=6).timestamp()),
+    start_time=int(
+        datetime.datetime(
+            year=2022, month=2, day=9, hour=10, minute=0, second=0
+        ).timestamp()
+    ),
 )
+zentraData = readings.response
 
-a = readings.response
-print(a.keys())
-print(len(a["device"]))
+tStamp = []
+solar = []
+precip = []
+wDirection = []
+wSpeed = []
+gustSpeed = []
+vaporPressure = []
+atmosPressure = []
+airTem = []
+maxPrecipRate = []
+rhTemp = []
+VPD = []
+
+# Extract time stamp, Precipitation, solar, temperature, and humidity
+for i in range(len(zentraData["device"]["timeseries"][0]["configuration"]["values"])):
+
+    tStamp.append(
+        zentraData["device"]["timeseries"][0]["configuration"]["values"][i][0]
+    )  # time stamp data
+
+    solar.append(
+        zentraData["device"]["timeseries"][0]["configuration"]["values"][i][3][0][
+            "value"
+        ]
+    )  # solar Radiation, 'unit':' W/m²'
+
+    precip.append(
+        zentraData["device"]["timeseries"][0]["configuration"]["values"][i][3][1][
+            "value"
+        ]
+    )  # Precipitation, 'unit':' mm'
+
+    airTem.append(
+        zentraData["device"]["timeseries"][0]["configuration"]["values"][i][3][7][
+            "value"
+        ]
+    )  # air temperature, 'unit'=' °C'
+
+    wDirection.append(
+        zentraData["device"]["timeseries"][0]["configuration"]["values"][i][3][4][
+            "value"
+        ]
+    )  # Wind Direction, 'units': ' °'
+
+    wSpeed.append(
+        zentraData["device"]["timeseries"][0]["configuration"]["values"][i][3][5][
+            "value"
+        ]
+    )  # wind speed, 'units': ' m/s'
+
+    vaporPressure.append(
+        zentraData["device"]["timeseries"][0]["configuration"]["values"][i][3][8][
+            "value"
+        ]
+    )  # vapor pressure, 'units': ' kPa'
+
+    atmosPressure.append(
+        zentraData["device"]["timeseries"][0]["configuration"]["values"][i][3][9][
+            "value"
+        ]
+    )  # Atmospheric Pressure, 'units': 'kPa'
+
+    VPD.append(
+        zentraData["device"]["timeseries"][0]["configuration"]["values"][i][3][14][
+            "value"
+        ]
+    )  # VPD, 'units': ' kPa'
+
+    rhTemp.append(
+        zentraData["device"]["timeseries"][0]["configuration"]["values"][i][3][13][
+            "value"
+        ]
+    )  # RH Senor Temp, 'units': ' °C'
+
+    gustSpeed.append(
+        zentraData["device"]["timeseries"][0]["configuration"]["values"][i][3][6][
+            "value"
+        ]
+    )  # Gust Speed, 'units': ' m/s'
+
+    maxPrecipRate.append(
+        zentraData["device"]["timeseries"][0]["configuration"]["values"][i][3][12][
+            "value"
+        ]
+    )  # Max Precip Rate, 'units': ' mm/h'
