9b19de84003f07a9e2e4516551d72ce1ac4b31f8
AbelTu0620 <j.tu20130227@gmail.com>
2022-02-11 15:54:34 +0000

    ""

--- GenerateRiverFlows/updateDB.py
+++ GenerateRiverFlows/updateDB.py
@@ -1,1 +1,13 @@
-import preZentra
+from manyfews.calculations.models import ZentraReading
+
+
+zentraData = ZentraReading(
+    device="",
+    relative_humidity="3",
+    air_temperature="4",
+    windspeed="5",
+    wind_direction="6",
+)
+
+
+zentraData.save()
