3fa0b3443857c5001fecf73939e5eccd7346eb35
AbelTu0620 <j.tu20130227@gmail.com>
2022-02-24 11:51:43 +0000

    "created two functions for pre-processing, 1. prepareGEFS: extract necessary GEFS forecast data sets. 2. prepareZentra: extract necessary Zentra observation data sets"

--- manyfews/calculations/tasks.py
+++ manyfews/calculations/tasks.py
@@ -22,17 +22,31 @@ def hello_celery():
     logger.info("Hello logging from celery!")
 
 
-# prepare GEFS data
-dt = float(settings.GEFS_TIME_STEP)
-forecastDays = int(settings.GEFS_FORECAST_DAYS)
-dataBaseWriter(dt=dt, forecastDays=forecastDays)
+@shared_task(name="calculations.prepareGEFS")
+def prepareGEFS():
+    """
+    This function is developed to extract necessary GEFS forecast data sets
+    into Database for running the River Flows model
+    """
+    # prepare GEFS data
+    dt = float(settings.GEFS_TIME_STEP)
+    forecastDays = int(settings.GEFS_FORECAST_DAYS)
+    dataBaseWriter(dt=dt, forecastDays=forecastDays)
 
-# prepare Zentra Cloud data
-stationSN = settings.STATION_SN
-zentraDevice = ZentraDevice.objects.get(device_sn=stationSN)
-sn = zentraDevice.device_sn
 
-# save into Data base
-backTime = float(settings.ZENTRA_BACKTIME)
+@shared_task(name="calculations.prepareZentra")
+def prepareZentra():
+    """
+    This function is developed to extract necessary Zentra cloud observation data sets
+    into Database for running the River Flows model
+    """
 
-zentraReader(backTime=backTime, stationSN=sn)
+    # prepare Zentra Cloud data
+    stationSN = settings.STATION_SN
+    zentraDevice = ZentraDevice.objects.get(device_sn=stationSN)
+    sn = zentraDevice.device_sn
+
+    # save into Data base
+    backTime = float(settings.ZENTRA_BACKTIME)
+
+    zentraReader(backTime=backTime, stationSN=sn)
