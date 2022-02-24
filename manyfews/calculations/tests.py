e2ec90f3ced026c44280410a43310d855fbaf10e
AbelTu0620 <j.tu20130227@gmail.com>
2022-02-24 14:34:17 +0000

    ""

--- manyfews/calculations/tests.py
+++ manyfews/calculations/tests.py
@@ -1,14 +1,3 @@
 from django.test import TestCase
 
-from .models import ZentraReading
-from .tasks import prepareZentra
-
-from django.test import TestCase
-
-
-class WeatherImportTests(TestCase):
-    def test_fetch_from_zentra(self):
-        prepareZentra()
-
-        readings = ZentraReading.objects.all()
-        assert len(readings) > 0
+# Create your tests here.
