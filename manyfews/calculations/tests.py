023dde76986059e31b21166d3c229756f6327fd7
Alison Clarke <alison.r.clarke@durham.ac.uk>
2022-02-24 14:25:04 +0000

    Example of test (probably won't work yet)

--- manyfews/calculations/tests.py
+++ manyfews/calculations/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import ZentraReading
+from .tasks import prepareZentra
+
+from django.test import TestCase
+
+
+class WeatherImportTests(TestCase):
+    def test_fetch_from_zentra(self):
+        prepareZentra()
+
+        readings = ZentraReading.objects.all()
+        assert len(readings) > 0
