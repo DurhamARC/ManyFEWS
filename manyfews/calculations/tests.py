0093f74da6186b67b7dc452c405418d0825a262f
AbelTu0620 <j.tu20130227@gmail.com>
2022-02-25 10:38:54 +0000

    "CI_test debugging."

--- manyfews/calculations/tests.py
+++ manyfews/calculations/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import ZentraDevice
 from .models import ZentraReading
 from .tasks import prepareZentra
 
@@ -9,6 +10,11 @@ from django.test import TestCase
 
 class WeatherImportTests(TestCase):
     def test_fetch_from_zentra(self):
+
+        # set up the info of Zentra Device for testing purpose.
+        zentraDevice = ZentraDevice("06-02047", location=Point(0, 0))
+        zentraDevice.save()
+
         prepareZentra()
 
 
