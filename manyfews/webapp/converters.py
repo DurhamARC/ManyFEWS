aa3c17169eea96105e4e3c5af57aba9fe5721ebd
Alison Clarke <alison.r.clarke@durham.ac.uk>
2022-03-14 16:01:30 +0000

    Add tests and error handling for converter

--- manyfews/webapp/converters.py
+++ manyfews/webapp/converters.py
@@ -2,15 +2,20 @@ from django.contrib.gis.geos import Polygon
 
 
 class BoundingBoxUrlParameterConverter:
-    regex = (
-        "\-?[0-9]+\.?[0-9]+,\-?[0-9]+\.?[0-9]+,\-?[0-9]+\.?[0-9]+,\-?[0-9]+\.?[0-9]+"
-    )
+    """Class to convert GIS coordinates representing a bounding box into a Polygon"""
+
+    regex = r"(\-?[0-9]+(\.?[0-9]+)?,){3}\-?[0-9]+(\.?[0-9]+)?"
 
     def to_python(self, value):
-        print(value)
         vals = value.split(",")
-        bounding_box = Polygon.from_bbox((float(val) for val in vals))
-        print(bounding_box)
+        if len(vals) != 4:
+            raise ValueError(f"Invalid coordinates {value}")
+
+        try:
+            bounding_box = Polygon.from_bbox((float(val) for val in vals))
+        except:
+            raise ValueError(f"Invalid coordinates {value}")
+
         return bounding_box
 
     def to_url(self, value):
--- manyfews/webapp/tests.py
+++ manyfews/webapp/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+import re
 
-# Create your tests here.
+from django.contrib.gis.geos import Polygon
+from django.test import TestCase
+from .converters import BoundingBoxUrlParameterConverter
+
+
+class WebAppTestCase(TestCase):
+    def test_bounding_box_url_parameter_converter(self):
+        converter = BoundingBoxUrlParameterConverter()
+
+        # Check conversion to bounding box
+        # Simple ints
+        bb = converter.to_python("1,2,3,4")
+        assert bb == Polygon.from_bbox((1, 2, 3, 4))
+
+        # Negative numbers and decimals
+        bb = converter.to_python("-1,2.345,-3.456789,4")
+        assert bb == Polygon.from_bbox((-1, 2.345, -3.456789, 4))
+
+        # Invalid types
+        self.assertRaises(ValueError, converter.to_python, "-1,2.345,-3.456789,4,5,6,7")
+        self.assertRaises(ValueError, converter.to_python, "not,a,valid,coordinate")
+
+        # Check regex works as expected
+        assert re.fullmatch(converter.regex, "1,2,3,4")
+        assert re.fullmatch(converter.regex, "-1,2.345,-3.456789,4")
+        assert not re.fullmatch(converter.regex, "1,2")
+        assert not re.fullmatch(converter.regex, "1,2-")
+        assert not re.fullmatch(converter.regex, "not,a,valid,coordinate")
