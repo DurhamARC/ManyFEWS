8d592b74a743d9109441b51d31db1cd30788f6bb
Alison Clarke <alison.r.clarke@durham.ac.uk>
2022-03-14 16:32:31 +0000

    Attempt a basic selenium test

--- manyfews/webapp/tests.py
+++ manyfews/webapp/tests.py
@@ -1,11 +1,13 @@
 import re
 
 from django.contrib.gis.geos import Polygon
-from django.test import TestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.test import TestCase, LiveServerTestCase
+from selenium.webdriver.chrome.webdriver import WebDriver
 from .converters import BoundingBoxUrlParameterConverter
 
 
-class WebAppTestCase(TestCase):
+class ConverterTestCase(TestCase):
     def test_bounding_box_url_parameter_converter(self):
         converter = BoundingBoxUrlParameterConverter()
 
@@ -28,3 +30,30 @@ class WebAppTestCase(TestCase):
         assert not re.fullmatch(converter.regex, "1,2")
         assert not re.fullmatch(converter.regex, "1,2-")
         assert not re.fullmatch(converter.regex, "not,a,valid,coordinate")
+
+
+class WebAppTestCase(StaticLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = WebDriver()
+        cls.selenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_home(self):
+        # Simple test that the home page loads without errors
+        self.selenium.get("%s%s" % (self.live_server_url, "/"))
+
+        risk_elements = self.selenium.find_elements_by_class_name("daily-risk")
+        assert len(risk_elements) == 7
+
+        risk_elements[1].click()
+
+        log = self.selenium.get_log("browser")
+        assert len(log) == 0, "Errors in browser log:\n" + "\n".join(
+            [f"{line['level']}: {line['message']}" for line in log]
+        )
