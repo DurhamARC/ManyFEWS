c224cbc64ec286815a09a9da995be1dacebc845b
Alison Clarke <alison.r.clarke@durham.ac.uk>
2022-03-14 12:09:47 +0000

    Load data for next 7 days if none currently present.

--- manyfews/webapp/load.py
+++ manyfews/webapp/load.py
@@ -18,26 +18,31 @@ start_location = Point(-7.065, 107.735)
 date = timezone.now().replace(hour=0, minute=0, second=0, microsecond=0)
 grid_size = 0.0005
 
-for d in range(5):
-    for i in range(100):
-        for j in range(150):
-            # Generate data for 1 point in 3
-            if random.random() < 0.33:
-                bounding_box = Polygon.from_bbox(
-                    (
-                        start_location.x + grid_size * i,
-                        start_location.y + grid_size * j,
-                        start_location.x + grid_size * (i + 1),
-                        start_location.y + grid_size * (j + 1),
+for d in range(7):
+    prediction_count = AggregatedDepthPrediction.objects.filter(
+        prediction_date=date
+    ).count()
+    if prediction_count == 0:
+        for i in range(100):
+            for j in range(150):
+                # Generate data for 1 point in 3
+                if random.random() < 0.33:
+                    bounding_box = Polygon.from_bbox(
+                        (
+                            start_location.x + grid_size * i,
+                            start_location.y + grid_size * j,
+                            start_location.x + grid_size * (i + 1),
+                            start_location.y + grid_size * (j + 1),
+                        )
                     )
-                )
-                median_depth = random.random()
-                prediction = AggregatedDepthPrediction(
-                    prediction_date=date,
-                    bounding_box=bounding_box,
-                    median_depth=median_depth,
-                    centile_25=median_depth - random.random() * median_depth,
-                    centile_75=median_depth + random.random() * 0.5,
-                )
-                prediction.save()
+                    median_depth = random.random()
+                    prediction = AggregatedDepthPrediction(
+                        prediction_date=date,
+                        bounding_box=bounding_box,
+                        median_depth=median_depth,
+                        centile_25=median_depth - random.random() * median_depth,
+                        centile_75=median_depth + random.random() * 0.5,
+                    )
+                    prediction.save()
+
     date += datetime.timedelta(days=1)
