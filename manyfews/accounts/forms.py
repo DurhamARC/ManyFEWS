fd8de0bcc1e976f3ef4e47f05937656fd54ee01a
Alison Clarke <alison.r.clarke@durham.ac.uk>
2022-03-21 16:33:27 +0000

    Add comments

--- manyfews/accounts/forms.py
+++ manyfews/accounts/forms.py
@@ -6,6 +6,10 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class EmailUserCreationForm(UserCreationForm):
+    """
+    Subclass Django's user creation form to use an email address as the username
+    """
+
     email = EmailField(required=True)
 
     class Meta:
@@ -13,6 +17,7 @@ class EmailUserCreationForm(UserCreationForm):
         fields = ("email", "password1", "password2")
 
     def clean_email(self):
+        # Ensure email address doesn't already exist
         email_address = self.cleaned_data["email"]
         if User.objects.filter(email=email_address).count():
             raise ValidationError("A user with this email address already exists.")
@@ -20,6 +25,7 @@ class EmailUserCreationForm(UserCreationForm):
         return email_address
 
     def save(self, commit=True):
+        # Save email address to username field as well
         user = super().save(commit=False)
         user.username = user.email
         if commit:
