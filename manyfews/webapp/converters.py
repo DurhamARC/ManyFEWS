80cf54ff8cf547176e26d67b7e4f404b4cdab4de
Alison Clarke <alison.r.clarke@durham.ac.uk>
2022-03-21 12:32:13 +0000

    Improved explanation of bounding box converter

--- manyfews/webapp/converters.py
+++ manyfews/webapp/converters.py
@@ -2,7 +2,13 @@ from django.contrib.gis.geos import Polygon
 
 
 class BoundingBoxUrlParameterConverter:
-    """Class to convert GIS coordinates representing a bounding box into a Polygon"""
+    """
+    Class to convert GIS coordinates representing a bounding box from a
+    string in the URL into a GEOS Polygon object.
+
+    Expects the coordinates to be comma-separated values representing NW and
+    SE coordinates, e.g. -7.05,107.73,-7.044,107.77
+    """
 
     regex = r"(\-?[0-9]+(\.?[0-9]+)?,){3}\-?[0-9]+(\.?[0-9]+)?"
 
